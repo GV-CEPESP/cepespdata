b7d6876e84601fb22f96fad94fdfed8739034b6a
RCCSilva <rafa_castrosilva@hotmail.com>
2018-03-09 10:11:43 -0300

    explorando os códigos inválidos de coligação

--- projeto_candidatos.R
+++ projeto_candidatos.R
@@ -4,81 +4,137 @@ library(tidyverse)
 library(httr)
 library(rvest)
 
-# 1. Carregando Banco -----------------------------------------------------
 
-get_dep_fed <- function(ano, cargo){
-  url_base <- "http://cepesp.io/api/consulta/candidatos"
-  
-  params <- list(
-    `cargo`              = cargo,
-    `ano`                = ano
-  )
-
-  data_frame <- GET(url_base, query = params) %>% 
-    content(type = "text/csv")
-}
+# 1. Carregando Banco - Candidatos ----------------------------------------
 
 anos <- c(seq(1998, 2014, by = 4))
 
 cargos <- c(1,3,5,6,7,8,11,13)
 
+colunas <- c("ANO_ELEICAO",
+             "NUM_TURNO",
+             "DESCRICAO_ELEICAO",
+             "SIGLA_UF",
+             "NOME_CANDIDATO",
+             "DES_SITUACAO_CANDIDATURA",
+             "NUMERO_PARTIDO",
+             "SIGLA_PARTIDO",
+             "CODIGO_LEGENDA",
+             "SIGLA_LEGENDA",
+             "COMPOSICAO_LEGENDA",
+             "NOME_COLIGACAO")
+  
+get_candidato <- function(ano, cargo, colunas){
+  url_base <- "http://cepesp.io/api/consulta/candidatos"
+  
+  params <- list(
+    `cargo`              = cargo,
+    `ano`                = ano)
+  
+  for(coluna in colunas){
+    i = length(params) + 1
+    
+    params[[i]] <- coluna
+    
+    names(params)[i] <- "selected_columns[]"
+  }
+
+  data_frame <- GET(url_base, query = params) %>% 
+    content(type = "text/csv")
+}
+
 candidatos_ls <- lst()
 
 for(ano in anos){
-  for(cargo in cargos){
     i = length(candidatos_ls) + 1
     
-    candidatos_ls[[i]] <- get_dep_fed(ano, cargo)
-  }
+    candidatos_ls[[i]] <- get_candidato(ano, cargo = 6, colunas)
 }
 
 candidatos_df <- bind_rows(candidatos_ls)
 
-glimpse(candidatos_ls)
+candidatos_df <- candidatos_df %>% 
+  filter(ANO_ELEICAO %in% c(2014, 2010, 2006))
 
-# 2. Consistência ---------------------------------------------------------
+# 2. Carregando Banco - Legandas ------------------------------------------
 
-candidatos_federais_df <- candidatos_federais_df %>% 
-  mutate(sit_canditado = case_when(DESC_SIT_TOT_TURNO %in% c("ELEITO", "ELEITO POR MÉDIA", "ELEITO POR QP") ~ "Eleito",
-                                   T                                                                        ~ "Não eleito"))
+colunas <- c("DATA_GERACAO",
+             "HORA_GERACAO",
+             "ANO_ELEICAO",
+             "NUM_TURNO",
+             "DESCRICAO_ELEICAO",
+             "SIGLA_UF",
+             "SIGLA_UE",
+             "CODIGO_CARGO",
+             "DESCRICAO_CARGO",
+             "TIPO_LEGENDA",
+             "NUMERO_PARTIDO",
+             "SIGLA_PARTIDO",
+             "SIGLA_COLIGACAO",
+             "NOME_COLIGACAO",
+             "COMPOSICAO_COLIGACAO",
+             "SEQUENCIAL_COLIGACAO")
 
-candidatos_federais_df %>% 
-  count(DESC_SIT_TOT_TURNO)
+legendas_ls <- lst()
 
-candidatos_federais_df %>% 
-  count(sit_canditado)
+get_legenda <- function(ano, cargo, colunas){
+  url_base <- "http://cepesp.io/api/consulta/legendas"
+  
+  params <- list(
+    `cargo`              = cargo,
+    `ano`                = ano)
+  
+  for(coluna in colunas){
+    i = length(params) + 1
+    
+    params[[i]] <- coluna
+    
+    names(params)[i] <- "selected_columns[]"
+  }
+  
+  data_frame <- GET(url_base, query = params) %>% 
+    content(type = "text/csv")
+}
 
-candidatos_federais_df %>% 
-  group_by(ANO_ELEICAO,SIGLA_UF) %>% 
-  count(sit_canditado) 
+for(ano in anos){
+  i = length(legendas_ls) + 1
+  
+  legendas_ls[[i]] <- get_legenda(ano, cargo = 6, colunas)
 
-# 3. Análise Exploratória -------------------------------------------------
+}
 
-candidatos_federais_df %>% 
-  group_by(ANO_ELEICAO,SIGLA_UF) %>% 
-  count(sit_canditado) %>% 
-  spread(key = sit_canditado, value = n) %>% 
-  mutate(total = Eleito + `Não eleito`) %>% 
-  gather(Eleito:`Não eleito`, key = sit_canditado, value = n) %>% 
-  mutate(prop = n / total) %>% 
-  filter(ANO_ELEICAO == 2014 & sit_canditado == "Eleito") %>% 
-  ggplot(mapping = aes(x = prop, y = reorder(SIGLA_UF, prop), color = sit_canditado)) +
-  geom_point()
+for(i in seq_along(legendas_ls)){
+  legendas_ls[[i]] <- legendas_ls[[i]] %>% 
+    mutate(SEQUENCIAL_COLIGACAO = parse_character(SEQUENCIAL_COLIGACAO))
+}
 
-prop_uf_df <- candidatos_federais_df %>% 
-  filter(DES_SITUACAO_CANDIDATURA == "DEFERIDO") %>% 
-  group_by(ANO_ELEICAO,SIGLA_UF) %>% 
-  count(sit_canditado) %>% 
-  spread(key = sit_canditado, value = n) %>% 
-  mutate(total = Eleito + `Não eleito`) %>% 
-  gather(Eleito:`Não eleito`, key = sit_canditado, value = n) %>% 
-  filter(ANO_ELEICAO == 2014) %>%
-  filter(sit_canditado == "Eleito") %>% 
-  mutate(prop       = n / total)
+legendas_df <- bind_rows(legendas_ls)
 
-prop_uf_df %>%
-  mutate(media_prop = mean(prop_uf_df$prop)) %>% 
-  gather(prop:media_prop, key = props,value = value) %>% 
-  ggplot(mapping = aes(x = value, y = reorder(SIGLA_UF, value), group = SIGLA_UF, color = props)) +
-  geom_point(size = 3) +
-  geom_line(size = 3, alpha = 0.3)
+legendas_df %>% 
+  count(TIPO_LEGENDA)
+
+legendas_df <- legendas_df %>% 
+  mutate(TIPO_LEGENDA = case_when(TIPO_LEGENDA == "PARTIDO ISOLADO" ~ "PARTIDO_ISOLADO",
+                                  T                                 ~ TIPO_LEGENDA))
+
+legendas_df %>% 
+  count(TIPO_LEGENDA)
+
+# 3. Consistência dos Dados -----------------------------------------------
+
+#'Verificando quais são os partidos com SEQUENCIAL_COLIGACAO e NOME_COLIGACAO %in% c("#NE#", "#NULO#")
+#'
+#' Aparenmente é possível agrupar por coligação mesmo quando o sequencial é inválido 
+#' 
+
+col_teste <- legendas_df %>% 
+  group_by(TIPO_LEGENDA) %>% 
+  count(SEQUENCIAL_COLIGACAO, nome_valido = !(NOME_COLIGACAO %in% c("#NE#", "#NULO#")))
+
+col_teste %>% 
+  arrange(desc(n))
+
+col_teste %>% 
+  filter(!nome_valido)
+
+
