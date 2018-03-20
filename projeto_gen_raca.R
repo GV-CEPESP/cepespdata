d28a0d81e96bdfcae43722d73f1c3845da7773aa
Rafael Coelho <rafael.coelho.x@gmail.com>
2018-03-20 11:43:07 -0300

    desenvolvimento de gráficos com votos e proporção de PPI, mulheres, escolaridade (superior ou não) por partido

--- projeto_gen_raca.R
+++ projeto_gen_raca.R
@@ -4,7 +4,6 @@ library(tidyverse)
 library(httr)
 library(genderBR)
 
-
 # 1. Carregando Bancos ----------------------------------------------------
 
 anos <- c(seq(1998, 2014, by = 4))
@@ -68,8 +67,49 @@ for(i in seq_along(candidatos_ls)){
 
 candidatos_df <- bind_rows(candidatos_ls)
 
+get_voto <- function(ano){
+  url_base <- "http://cepesp.io/api/consulta/tse"
+  
+  params <- list(
+    `cargo`              = 6,
+    `ano`                = ano,
+    `agregacao_regional` = 2,
+    `agregacao_politica` = 2,
+    `selected_columns[]`   = "ANO_ELEICAO",
+    `selected_columns[]`   = "NUM_TITULO_ELEITORAL_CANDIDATO",
+    `selected_columns[]`   = "UF",
+    `selected_columns[]`   = "QTDE_VOTOS")
+  
+  data_frame <- GET(url_base, query = params) %>% 
+    content(type = "text/csv")
+}
 
-# 2. Consistência ---------------------------------------------------------
+votos_ls <- lst()
+
+for(ano in anos){
+  i = length(votos_ls) + 1
+  
+  votos_ls[[i]] <- get_voto(ano)
+}
+
+votos_df <- bind_rows(votos_ls)
+
+# 2. Criando Variáveis ----------------------------------------------------
+
+#2.1. Banco de Votos
+votos_df %>% 
+  count(NUM_TITULO_ELEITORAL_CANDIDATO) %>% 
+  arrange(desc(n))
+
+votos_df <- votos_df %>%
+  mutate(NUM_TITULO_ELEITORAL_CANDIDATO = ifelse(NUM_TITULO_ELEITORAL_CANDIDATO %in% c("#NULO#", "#NI#"),
+                                                 NA, NUM_TITULO_ELEITORAL_CANDIDATO))
+
+#2.2. Merge
+
+candidatos_df <- candidatos_df %>% 
+  left_join(votos_df, by = c("ANO_ELEICAO", "SIGLA_UF" = "UF", "NUM_TITULO_ELEITORAL_CANDIDATO"))
+
 
 candidatos_df %>% 
   count(DES_SITUACAO_CANDIDATURA) %>% 
@@ -78,43 +118,78 @@ candidatos_df %>%
 candidatos_df %>% 
   count(DESCRICAO_SEXO)
 
-candidatos_df$DESCRICAO_SEXO[candidatos_df$DESCRICAO_SEXO == "NÃO INFORMADO"] <- NA
+candidatos_df <- candidatos_df %>% 
+  mutate(sexo = fct_recode(DESCRICAO_SEXO,
+                           "Mulheres" = "FEMININO",
+                           "Homens"   = "MASCULINO",
+                           NULL       = "NÃO INFORMADO"))
 
 candidatos_df %>% 
-  count(DESCRICAO_GRAU_INSTRUCAO)
+  count(sexo)
 
 candidatos_df %>% 
   count(DESCRICAO_COR_RACA)
 
+candidatos_df <- candidatos_df %>% 
+  mutate(raca = fct_recode(DESCRICAO_COR_RACA,
+                           "Branca"  = "BRANCA",
+                           "Amarela" = "AMARELA",
+                           "Indígena"= "INDÍGENA",
+                           "Parda"   = "PARDA",
+                           "Preta"   = "PRETA",
+                           NULL      = "#NE#"))
+
 candidatos_df %>% 
-  ggplot(mapping = aes(ANO_ELEICAO, fill = DESCRICAO_COR_RACA)) +
-  geom_bar()
+  count(raca)
+
+candidatos_df %>% 
+  count(DESCRICAO_GRAU_INSTRUCAO)
+
+candidatos_df <- candidatos_df %>%
+  mutate(escolaridade = fct_collapse(DESCRICAO_GRAU_INSTRUCAO,
+                                     "Não Superior" = c("1º GRAU COMPLETO",
+                                                        "1º GRAU INCOMPLETO", 
+                                                        "2º GRAU COMPLETO",
+                                                        "2º GRAU INCOMPLETO",
+                                                        "ANALFABETO",
+                                                        "ENSINO FUNDAMENTAL COMPLETO",
+                                                        "ENSINO FUNDAMENTAL INCOMPLETO",
+                                                        "ENSINO MÉDIO COMPLETO",
+                                                        "ENSINO MÉDIO INCOMPLETO",  
+                                                        "FUNDAMENTAL COMPLETO",    
+                                                        "FUNDAMENTAL INCOMPLETO",     
+                                                        "LÊ E ESCREVE",
+                                                        "MÉDIO COMPLETO",            
+                                                        "MÉDIO INCOMPLETO",          
+                                                        "NÃO INFORMADO"),
+                                     "Superior (Incompleto + Completo)" = c("SUPERIOR COMPLETO",
+                                                                            "SUPERIOR INCOMPLETO")))
+
+candidatos_df %>% 
+  count(escolaridade)
 
 candidatos_df %>% 
   count(DESPESA_MAX_CAMPANHA)
-
-candidatos_df <- candidatos_df %>% 
-  mutate(DESPESA_MAX_CAMPANHA = ifelse(DESPESA_MAX_CAMPANHA == -1, NA, DESPESA_MAX_CAMPANHA))
-
-candidatos_df %>% 
-  ggplot(mapping = aes(x = DESPESA_MAX_CAMPANHA)) +
-  geom_histogram()
 
 candidatos_df %>% 
   count(DESPESA_MAX_CAMPANHA) %>% 
   arrange(desc(DESPESA_MAX_CAMPANHA))
 
-# 3. Gráficos -------------------------------------------------------------
 candidatos_df %>% 
-  filter(!is.na(DESCRICAO_SEXO)) %>% 
-  ggplot(mapping = aes(x = ANO_ELEICAO, fill = DESCRICAO_SEXO)) +
+  ggplot(mapping = aes(x = DESPESA_MAX_CAMPANHA)) +
+  geom_histogram()
+
+# 3. Gráficos -------------------------------------------------------------
+
+candidatos_df %>% 
+  filter(!is.na(sexo)) %>% 
+  ggplot(mapping = aes(x = ANO_ELEICAO, fill = sexo)) +
   geom_bar(position = "fill") +
   geom_hline(yintercept = 0.5) +
   scale_x_continuous(breaks = anos) +
   theme_minimal() +
   theme(legend.position = "bottom") +
-  scale_fill_discrete(labels = c("Mulheres", "Homens")) +
-  labs(title = "Proporção de Mulheres Candidatas",
+  labs(title = "Candidatos em função de sexo",
        fill = "Sexo",
        x = "Ano",
        y = "Proporção")
@@ -124,8 +199,8 @@ for(ano in anos){
     filter(ANO_ELEICAO == ano) %>% 
     group_by(SIGLA_PARTIDO, ANO_ELEICAO) %>% 
     summarise(n   = n(),
-              fem = sum(DESCRICAO_SEXO == "FEMININO", na.rm = T),
-              mas = sum(DESCRICAO_SEXO == "MASCULINO", na.rm = T)) %>% 
+              fem = sum(sexo == "Mulheres", na.rm = T),
+              mas = sum(sexo == "Homens", na.rm = T)) %>% 
     mutate(prop_m = fem/n) %>%
     gather(fem:mas, key = "gen", value = "quanti") %>% 
     ggplot(mapping = aes(x = reorder(SIGLA_PARTIDO, prop_m), y = quanti, fill = gen)) +
@@ -135,7 +210,7 @@ for(ano in anos){
     theme_minimal() +
     theme(legend.position = "bottom") +
     scale_fill_discrete(labels = c("Mulheres", "Homens")) +
-    labs(title = str_c("Proporção de Mulheres por Partido: ", ano),
+    labs(title = str_c("Proporção de Homens e Mulheres por Partido: ", ano),
          fill = "Sexo",
          x = "Partido",
          y = "Proporção")
@@ -144,21 +219,18 @@ for(ano in anos){
 }
 
 candidatos_df %>% 
-  count(DESCRICAO_COR_RACA)
-
-candidatos_df %>% 
   filter(ANO_ELEICAO == 2014) %>% 
   group_by(SIGLA_PARTIDO) %>% 
   summarise(n = n(),
-            amarela  = sum(DESCRICAO_COR_RACA == "AMARELA"),
-            branca   = sum(DESCRICAO_COR_RACA == "BRANCA"),
-            indigena = sum(DESCRICAO_COR_RACA == "INDÍGENA"),
-            parda    = sum(DESCRICAO_COR_RACA == "PARDA"),
-            preta    = sum(DESCRICAO_COR_RACA == "PRETA"),
-            ppi      = sum(DESCRICAO_COR_RACA %in% c("INDÍGENA", "PARDA", "PRETA"))) %>% 
+            amarela  = sum(raca == "Amarela"),
+            branca   = sum(raca == "Branca"),
+            indigena = sum(raca == "Indígena"),
+            parda    = sum(raca == "Parda"),
+            preta    = sum(raca == "Preta"),
+            ppi      = sum(raca %in% c("Indígena", "Parda", "Preta"))) %>% 
   mutate(prop_ppi = ppi / n) %>% 
-  gather(amarela:preta, key = "DESCRICAO_COR_RACA", value = "quanti") %>% 
-  ggplot(mapping = aes(reorder(SIGLA_PARTIDO, prop_ppi), quanti, fill = DESCRICAO_COR_RACA)) +
+  gather(amarela:preta, key = "raca", value = "quanti") %>% 
+  ggplot(mapping = aes(reorder(SIGLA_PARTIDO, prop_ppi), quanti, fill = raca)) +
   geom_bar(position = "fill", stat = "identity") +
   coord_flip() +
   scale_fill_discrete(labels = c("Amarela", "Branca", "Indígena", "Parda", "Preta")) +
@@ -170,3 +242,44 @@ candidatos_df %>%
        fill = "Cor/Raça")
   
 ggsave("diff_rac.png", width = 8.0, height = 5.0)
+
+candidatos_df %>% 
+  filter(!is.na(escolaridade)) %>% 
+  ggplot(mapping = aes(x = ANO_ELEICAO, fill =  escolaridade)) +
+  geom_bar(position = "fill")
+
+candidatos_df %>% 
+  filter(DES_SITUACAO_CANDIDATURA == "DEFERIDO") %>% 
+  filter(ANO_ELEICAO == 2014) %>% 
+  group_by(SIGLA_PARTIDO) %>% 
+  summarise(ppi   = sum(raca %in% c("Indígena", "Preta", "Parda")),
+            n     = n(),
+            votos = sum(QTDE_VOTOS, na.rm = T)) %>% 
+  mutate(prop_ppi = ppi / n) %>% 
+  ggplot(mapping = aes(x = prop_ppi, y = votos)) +
+  geom_point()
+
+candidatos_df %>% 
+  filter(DES_SITUACAO_CANDIDATURA == "DEFERIDO") %>% 
+  filter(ANO_ELEICAO == 2014) %>% 
+  group_by(SIGLA_PARTIDO) %>% 
+  summarise(mulh = sum(sexo == "Mulheres"),
+            n    = n(),
+            votos= sum(QTDE_VOTOS, na.rm = T)) %>% 
+  mutate(prop_m = mulh / n) %>% 
+  arrange(desc(prop_m)) %>% 
+  ggplot(mapping = aes(x = prop_m, y = votos)) +
+  geom_point()
+
+candidatos_df %>% 
+  filter(DES_SITUACAO_CANDIDATURA == "DEFERIDO") %>% 
+  filter(ANO_ELEICAO == 2014) %>% 
+  group_by(SIGLA_PARTIDO) %>% 
+  summarise(mulh = sum(escolaridade == "Não Superior"),
+            n    = n(),
+            votos= sum(QTDE_VOTOS, na.rm = T)) %>% 
+  mutate(prop_m = mulh / n) %>% 
+  arrange(desc(prop_m)) %>% 
+  ggplot(mapping = aes(x = prop_m, y = votos)) +
+  geom_point()
+
