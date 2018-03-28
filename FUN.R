2e3d1ef53c7af5b184087d537652ba04394a4331
Rafael de Castro Coelho Silva <rafael.coelho@fgv.br>
2018-03-28 12:50:55 -0300

    troquei a ordem dos argumentos

--- FUN.R
+++ FUN.R
@@ -7,7 +7,7 @@ get_params <- function(params, colunas){
   return(params)
 }
 
-get_candidatos <- function(cargo, ano, colunas){
+get_candidatos <- function(ano, cargo, colunas){
   url_base <- "http://cepesp.io/api/consulta/candidatos"
   
   params <- lst(
@@ -21,18 +21,20 @@ get_candidatos <- function(cargo, ano, colunas){
     content(type = "text/csv")
 }
 
-get_votos <- function(cargo, ano, colunas, agre_reg, agre_pol){
+get_votos <- function(ano, cargo, colunas, agre_reg, agre_pol){
   url_base <- "http://cepesp.io/api/consulta/tse"
   
   params <- lst(
     `cargo`              = cargo,
     `ano`                = ano,
     `agregacao_regional` = agre_reg,
-    `agregacao_politica` = agre_pol
+    `agregacao_politica` = agre_pol,
+    `brancos`            = 1,
+    `nulos`              = 1
   )
   
   params <- get_params(params, colunas)
   
-  GET(url_base, query = params) %>% 
+  data <- GET(url_base, query = params) %>% 
     content(type = "text/csv")
 }
